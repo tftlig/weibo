6c303dbd80d8f6ce817a3ebf7d4c99afb45715a2
vagrant <vagrant@homestead>
2021-12-27 03:23:46 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//首页、帮助页、关于页路由
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
-
+//登录路由
 Route::get('signup','UsersController@create')->name('signup');
-
+//用户资源自由
 Route::resource('users','UsersController');
+
+//会话路由
+Route::get('login','SessionsController@create')->name('login');
+Route::post('login','SessionsController@store')->name('login');
+Route::delete('logout','SessionsController@destroy')->name('logout');
