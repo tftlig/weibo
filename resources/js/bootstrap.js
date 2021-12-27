a50e68c95c1186bfcb14db896bcbc6df185a012f
vagrant <vagrant@homestead>
2021-12-27 13:00:52 +0000

    登陆成功后导航逻辑

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,9 @@
 window._ = require('lodash');
 
 try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
     require('bootstrap');
 } catch (e) {}
 
@@ -13,6 +16,7 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
